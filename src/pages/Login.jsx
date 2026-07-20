043b2fdfeae46501397b8effcab1de9786afb38c
mrpiyalst <mrpiyalst@gmail.com>
2026-07-20 11:16:34 +0600

    feat: install material-ui

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-full h-screen bg-primary/10 flex items-center justify-center'>
+      <div className='w-160 bg-white h-40 shadow p-8 rounded-xl'>
+        <h2>Welcome To Login</h2>
+        <div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
