c689bff6f53eba80e85d728736e3812be005783e
mrpiyalst <mrpiyalst@gmail.com>
2026-07-21 19:22:50 +0600

    feat: add user_profile components

---
+++ src/components/UserProfle.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const UserProfle = () => {
+  return (
+    <>
+      <div className='cursor-pointer flex justify-between py-3 px-2 bg-primary/10 hover:bg-primary/20 transition-all duration-300 rounded-md'>
+        <div className='flex items-center gap-2'>
+          <div className='w-12'>
+            <img src={assets.user_icon} alt="" />
+          </div>
+          <div>
+            <p className='text-base text-gray-800 font-normal'>Naruto Uzumaki</p>
+          </div>
+        </div>
+        <div className='flex gap-2 items-center'>
+          <button className='px-3 py-1 bg-primary/90 rounded-full text-xs text-gray-100 cursor-pointer'>Accept</button>
+          <button className='px-3 py-1 bg-primary/90 rounded-full text-xs text-gray-100 cursor-pointer'>Cencel</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default UserProfle
