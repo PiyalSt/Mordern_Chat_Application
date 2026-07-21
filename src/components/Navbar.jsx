c6e300d21bcdf0ef56af6a2cd146e0b75a9f36ef
mrpiyalst <mrpiyalst@gmail.com>
2026-07-21 19:29:59 +0600

    fix: navbar margin, padding, font-size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ import { BsChatDotsFill } from "react-icons/bs";
 const Navbar = () => {
   return (
     <>
-      <div className="w-fit h-screen px-8 py-12 bg-primary/10 flex flex-col justify-between items-center">
-        <div className="flex flex-col  items-center">
+      <div className="w-fit h-screen px-8 py-4 bg-primary/10 flex flex-col justify-between items-center">
+        <div className="flex flex-col items-center">
           <div className="w-12">
             <img src={assets.chat_logo} alt="" />
           </div>
-          <div className="flex flex-col gap-1 mt-12">
+          <div className="flex flex-col gap-1 mt-6">
             <div className="p-4 hover:bg-primary/60 rounded-full cursor-pointer transition-all duration-300 group">
               <SiHomeadvisor className="text-2xl text-gray-800 group-hover:text-white" />
             </div>
@@ -24,7 +24,7 @@ const Navbar = () => {
           <div className="w-12 bg-white p-2 rounded-full cursor-pointer">
             <img src={assets.profile_icon} alt="" />
           </div>
-          <p className="text-base text-gray-900 capitalize font-medium">
+          <p className="w-full text-center text-sm text-gray-900 capitalize font-medium">
             Piyal hasan
           </p>
         </div>
